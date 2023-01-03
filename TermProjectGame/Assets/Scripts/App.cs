89541aa1e4a87159db4f4cb9ac483f8e9e2a28ca
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2023-01-03 16:15:08 +0200

    +Created unity project +basic project and scene structure

---
+++ TermProjectGame/Assets/Scripts/App.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using World;
+using Systems;
+
+public class App : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectsContainer objectsContainer;
+    [SerializeField]
+    private InputSystem inputSystem;
+}
---
+++ TermProjectGame/Assets/Scripts/Systems/InputSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systems
+{
+    public class InputSystem : MonoBehaviour
+    {
+        public void Init()
+        {
+
+        }
+    }
+}
+
